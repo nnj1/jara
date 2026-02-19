bb864b3c69e9ff34a5865e251bede94f57de182b
shrimpboyho <shrimpboyhoftw@gmail.com>
2026-02-19 14:32:24 -0500

    working on stuff

--- src/lightning.gd
+++ src/lightning.gd
@@ -3,23 +3,43 @@ extends Node3D
 @onready var cylinder = $MeshInstance3D
 @export var ray: RayCast3D
 
+@onready var lightning_sound: AudioStreamPlayer3D = $AudioStreamPlayer3D
+
 var is_active: bool = false
+var attack_timeout: float = 0.5
+var attack_timer: float = attack_timeout
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 	
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if is_active:
+		if not lightning_sound.playing:
+			lightning_sound.play()
 		if ray.is_colliding():
 			var hit_point = ray.get_collision_point()
 			strike_at(hit_point)
 			# 2. Calculate the distance from the RayCast origin to that point
 			var _distance = ray.global_position.distance_to(hit_point)
-			# Optional: Get the actual object hit
-			var _body = ray.get_collider()
+			# apply_damage
+			var target = ray.get_collider()
+			if target is Player:
+				# TODO: only do if PVP friendly fire is turned on
+				pass
+			if target is DumbEnemy:
+				var health_component = target.get_node_or_null('HealthComponent')
+				if health_component:
+					attack_timer -= delta
+					if attack_timer < 0:
+						var damage_amount = randi_range(1,5)
+						health_component.take_damage(damage_amount, true if damage_amount > 3 else false)
+						attack_timer = attack_timeout
 		else:
 			strike_at(ray.target_position)
+	else:
+		lightning_sound.stop()
 		
 func strike_at(target_pos: Vector3):
 	var dist = (target_pos - self.global_position).length()
--- src/skeleton.gd
+++ src/skeleton.gd
@@ -183,7 +183,6 @@ func _on_died():
 	# Disable collision so the player doesn't trip over a corpse
 	collision_layer = 0
 	collision_mask = 1 # Keep mask for ground floor only
-	anim_player.speed_scale = 2 # death animation is a little slow so speed up the player
 	safe_play("die")
 	
 	# Optional: Remove the enemy after some time
@@ -192,6 +191,8 @@ func _on_died():
 
 func safe_play(anim_name: String):
 	var n = "skeleton_stuff/rig_rig_" + anim_name
+	if anim_name == 'die':
+		anim_player.speed_scale = 2 # death animation is a little slow so speed up the player
 	if anim_player.has_animation(n):
 		if anim_player.current_animation != n:
 			anim_player.play(n)
