793c8d399ae70f8cd4ad316b6e2bc0da30d29145
shrimpboyho <shrimpboyhoftw@gmail.com>
2026-02-28 18:09:08 -0500

    more updates

--- src/spikes.gd
+++ src/spikes.gd
@@ -1,12 +1,17 @@
 extends Node3D
 
-@onready var static_body: StaticBody3D = $Cube_048/StaticBody3D
+#TODO: make the damage continuous
+
+@onready var damage_zone: Area3D = $Area3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is Player:
+		body.get_node('HealthComponent').take_damage_synced(10, false)
